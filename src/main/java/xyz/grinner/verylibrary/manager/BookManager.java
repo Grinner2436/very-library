6c7841a8e0a26ed781c35d7bfa569914b5d8459b
CK <1078599145@qq.com>
2019-12-11 23:51:13 +0800

    同步代码

---
+++ src/main/java/xyz/grinner/verylibrary/manager/BookManager.java
@@ -0,0 +1,20 @@
+package xyz.grinner.verylibrary.manager;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.springframework.stereotype.Component;
+import xyz.grinner.verylibrary.schema.Page;
+
+@Component
+public class BookManager {
+
+    public Page parsePDF(){
+//        PDDocument pdDocument = new PDDocument();
+//        int numberOfPages = pdDocument.getNumberOfPages();
+//        for(int pageNum : numberOfPages){
+//            PDPage pdPage = pdDocument.getPage(pageNum);
+//            pdPage.getContentStreams().forEachRemaining(page ->{
+//            });
+//        }
+    }
+}
